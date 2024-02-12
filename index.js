0e529f6ebbb2c6f1fc88eacb9bf2d29aa6dbd67c
HarrisLich <112398044+HarrisLich@users.noreply.github.com>
2024-02-12 10:43:08 -0500

    api update

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ mongoose.set({strictQuery: false})
 mongoose.connect(process.env.dbString)
 
 app.use(cors())
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.get("/", (req,res)=>{
     res.json({msg: "testing!!"})
